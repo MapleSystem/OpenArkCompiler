bd2039008aed4117a9d1beb1151844c3c961c0ea
William Chen <william.chen@futurewei.com>
2021-06-30 17:15:13 -0700

    Infrastructure for vreg rename for loops for C.  Option --vreg-rename

--- src/mapleall/maple_be/src/cg/cg_option.cpp
+++ src/mapleall/maple_be/src/cg/cg_option.cpp
@@ -65,6 +65,7 @@ bool CGOptions::doCFGO = false;
 bool CGOptions::doICO = false;
 bool CGOptions::doStoreLoadOpt = false;
 bool CGOptions::doGlobalOpt = false;
+bool CGOptions::doVregRename = false;
 bool CGOptions::doMultiPassColorRA = true;
 bool CGOptions::doPrePeephole = false;
 bool CGOptions::doPeephole = false;
@@ -120,6 +121,7 @@ enum OptionIndex : uint64 {
   kPeep,
   kPreSchedule,
   kSchedule,
+  kVregRename,
   kMultiPassRA,
   kWriteRefFieldOpt,
   kDumpOlog,
@@ -407,6 +409,16 @@ const Descriptor kUsage[] = {
     kArgCheckPolicyBool,
     "  --schedule                  \tPerform scheduling\n"
     "  --no-schedule\n",
+    "mplcg",
+    {} },
+  { kVregRename,
+    kEnable,
+    "",
+    "vreg-rename",
+    kBuildTypeExperimental,
+    kArgCheckPolicyBool,
+    "  --vreg-rename                  \tPerform rename of long live range around loops in coloring RA\n"
+    "  --no-vreg-rename\n",
     "mplcg",
     {} },
   { kMultiPassRA,
@@ -1300,6 +1312,9 @@ bool CGOptions::SolveOptions(const std::vector<Option> &opts, bool isDebug) {
         break;
       case kSchedule:
         (opt.Type() == kEnable) ? EnableSchedule() : DisableSchedule();
+        break;
+      case kVregRename:
+        (opt.Type() == kEnable) ? EnableVregRename() : DisableVregRename();
         break;
       case kMultiPassRA:
         (opt.Type() == kEnable) ? EnableMultiPassColorRA() : DisableMultiPassColorRA();
